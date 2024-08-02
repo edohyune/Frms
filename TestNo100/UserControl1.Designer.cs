3f0d73219ec64cd9fdf45e279188e76212116013
nophominkham <165004618+nophominkham@users.noreply.github.com>
2024-08-02 18:39:05 +0700

    Create Test1

--- TestNo100/UserControl1.Designer.cs
+++ TestNo100/UserControl1.Designer.cs
@@ -28,11 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            ucSplit1 = new EpicV004.Ctrls.UCSplit();
+            ucPanel1 = new EpicV004.Ctrls.UCPanel();
+            ucCodeBox1 = new EpicV004.Ctrls.UCCodeBox();
+            ucGridSet1 = new EpicV004.Ctrls.UCGridSet();
+            ((System.ComponentModel.ISupportInitialize)ucSplit1).BeginInit();
+            ucSplit1.Panel1.SuspendLayout();
+            ucSplit1.Panel2.SuspendLayout();
+            ucSplit1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ucPanel1).BeginInit();
+            ucPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // ucSplit1
+            // 
+            ucSplit1.Dock = DockStyle.Fill;
+            ucSplit1.Location = new Point(0, 0);
+            ucSplit1.Name = "ucSplit1";
+            ucSplit1.Orientation = Orientation.Horizontal;
+            // 
+            // ucSplit1.Panel1
+            // 
+            ucSplit1.Panel1.Controls.Add(ucPanel1);
+            // 
+            // ucSplit1.Panel2
+            // 
+            ucSplit1.Panel2.Controls.Add(ucGridSet1);
+            ucSplit1.Size = new Size(800, 450);
+            ucSplit1.SplitterDistance = 118;
+            ucSplit1.TabIndex = 0;
+            ucSplit1.TitleWidth = 121;
+            // 
+            // ucPanel1
+            // 
+            ucPanel1.Controls.Add(ucCodeBox1);
+            ucPanel1.Dock = DockStyle.Fill;
+            ucPanel1.Location = new Point(0, 0);
+            ucPanel1.Name = "ucPanel1";
+            ucPanel1.Size = new Size(800, 118);
+            ucPanel1.TabIndex = 0;
+            ucPanel1.Text = "ucPanel1";
+            // 
+            // ucCodeBox1
+            // 
+            ucCodeBox1.ControlHeight = 21;
+            ucCodeBox1.ControlWidth = 200;
+            ucCodeBox1.FontColor = Color.FromArgb(40, 40, 40);
+            ucCodeBox1.FontFace = "Tahoma";
+            ucCodeBox1.FontSize = 9F;
+            ucCodeBox1.ImeMode = ImeMode.NoControl;
+            ucCodeBox1.Location = new Point(5, 46);
+            ucCodeBox1.Name = "ucCodeBox1";
+            ucCodeBox1.Size = new Size(200, 20);
+            ucCodeBox1.TabIndex = 0;
+            ucCodeBox1.Text = "ucCodeBox1";
+            ucCodeBox1.TextAlignment = DevExpress.Utils.HorzAlignment.Near;
+            ucCodeBox1.Title = "Search";
+            ucCodeBox1.TitleAlignment = DevExpress.Utils.HorzAlignment.Default;
+            ucCodeBox1.TitleWidth = 100;
+            // 
+            // ucGridSet1
+            // 
+            ucGridSet1.Dock = DockStyle.Fill;
+            ucGridSet1.Location = new Point(0, 0);
+            ucGridSet1.Name = "ucGridSet1";
+            ucGridSet1.Size = new Size(800, 328);
+            ucGridSet1.TabIndex = 0;
+            // 
+            // UserControl1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(ucSplit1);
+            Name = "UserControl1";
+            Size = new Size(800, 450);
+            ucSplit1.Panel1.ResumeLayout(false);
+            ucSplit1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)ucSplit1).EndInit();
+            ucSplit1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)ucPanel1).EndInit();
+            ucPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private EpicV004.Ctrls.UCSplit ucSplit1;
+        private EpicV004.Ctrls.UCPanel ucPanel1;
+        private EpicV004.Ctrls.UCCodeBox ucCodeBox1;
+        private EpicV004.Ctrls.UCGridSet ucGridSet1;
     }
 }
