6a7fd4ccfaa96c85e3879b8d66b6ad6d5420b724
soukkp <soukkp@gmail.com>
2024-08-02 18:46:50 +0700

    Update version DPM200

--- DPM200/obj/Debug/net8.0-windows/DPM200.AssemblyInfo.cs
+++ DPM200/obj/Debug/net8.0-windows/DPM200.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DPM200")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f454c840c70cf04d79911f5efca05b44104dc251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3158fe5f56050b3b5373c41b3678b3dff79c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("DPM200")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DPM200")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
