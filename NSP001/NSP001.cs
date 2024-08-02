aab85753934c3e5b181cccb2a5a1c54f766dfdbe
nophominkham <165004618+nophominkham@users.noreply.github.com>
2024-08-02 18:35:35 +0700

    Create from test

---
+++ NSP001/NSP001.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace EpicV004.Frms
+{
+    public partial class NSP001 : FrmBase
+    {
+        public NSP001()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NTESTDVP/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicV004.Frms
+{
+    public class NTESTDVP_G_10 : EpicV004.Libs.MdlBase
+    {
+        private int _ID;
+        public int ID
+        {
+            get => _ID;
+            set => Set(ref _ID, value);
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+
+        private int _Age;
+        public int Age
+        {
+            get => _Age;
+            set => Set(ref _Age, value);
+        }
+
+        private int _PID;
+        public int PID
+        {
+            get => _PID;
+            set => Set(ref _PID, value);
+        }
+
+    }
+}
