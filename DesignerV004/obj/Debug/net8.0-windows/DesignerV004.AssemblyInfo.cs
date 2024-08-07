ab5fdf2e903f5459abd1a8e232b6b0214ad95758
edohyune <edohyune@gmail.com>
2024-08-07 18:27:50 +0700

    202408071827

--- DesignerV004/obj/Debug/net8.0-windows/DesignerV004.AssemblyInfo.cs
+++ DesignerV004/obj/Debug/net8.0-windows/DesignerV004.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignerV004")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94874140bdcaa290798d544c600d9554547dd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ffab16577fde04afed5bc2734af008750a32f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignerV004")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignerV004")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TESTFRMS/obj/Debug/net8.0-windows/TESTFRMS.AssemblyInfo.cs
+++ TESTFRMS/obj/Debug/net8.0-windows/TESTFRMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TESTFRMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75104b4281a85325e337e8b4f9cc6b563bf79ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ffab16577fde04afed5bc2734af008750a32f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TESTFRMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TESTFRMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
