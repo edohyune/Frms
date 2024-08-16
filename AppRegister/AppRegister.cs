774de44a92eb07cdc46b633bd13adcaf462ee336
nophominkham <165004618+nophominkham@users.noreply.github.com>
2024-08-16 13:57:44 +0700

    New create

---
+++ AppRegister/AppRegister.cs
@@ -0,0 +1,52 @@
+﻿namespace EpicV004.Frms
+
+{
+    public partial class AppRegister : FrmBase
+    {
+        public AppRegister()
+        {
+            InitializeComponent();
+        }
+
+        protected override void BarButtonAction(string frm, string action)
+        {
+
+            string searchStr = g10.GetText("applicant_id").ToString(); // Get the ID of the current row
+            if (this.Name == frm)
+            {
+                switch (action)
+                {
+                    case "Save":
+                        if (this.Save())
+                        {
+                            g10.Open(); // Reload the grid data
+                            g10.FocuseByFindedValue("applicant_id", searchStr); // Focus the row with the saved ID
+
+                            g20.Open();
+                            g20.FocuseByFindedValue("applicant_id", searchStr);
+
+                            g30.Open();
+                            g30.FocuseByFindedValue("applicant_id", searchStr);
+
+                            g40.Open();
+                            g40.FocuseByFindedValue("applicant_id", searchStr);
+
+                            g50.Open();
+                            g50.FocuseByFindedValue("applicant_id", searchStr);
+                        }
+                        break;
+                    case "Open":
+                        this.Open();
+                        break;
+                    case "New":
+                        NewWorkSet("f10");
+                        break;
+                    default:
+                        base.BarButtonAction(frm, action);
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ AppRegister/HB130.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicV004.Frms
+{
+    public class AppRegister_G30 : EpicV004.Libs.MdlBase
+    {
+        private long _pid;
+        public long pid
+        {
+            get => _pid;
+            set => Set(ref _pid, value);
+        }
+
+        private string _applicant_id;
+        public string applicant_id
+        {
+            get => _applicant_id;
+            set => Set(ref _applicant_id, value);
+        }
+
+        private int _seq;
+        public int seq
+        {
+            get => _seq;
+            set => Set(ref _seq, value);
+        }
+
+        private string _level;
+        public string level
+        {
+            get => _level;
+            set => Set(ref _level, value);
+        }
+
+        private string _Institution_name;
+        public string Institution_name
+        {
+            get => _Institution_name;
+            set => Set(ref _Institution_name, value);
+        }
+
+        private string _year_end;
+        public string year_end
+        {
+            get => _year_end;
+            set => Set(ref _year_end, value);
+        }
+
+        private string _major;
+        public string major
+        {
+            get => _major;
+            set => Set(ref _major, value);
+        }
+
+        private string _quali_rcvd;
+        public string quali_rcvd
+        {
+            get => _quali_rcvd;
+            set => Set(ref _quali_rcvd, value);
+        }
+
+        private decimal _grade;
+        public decimal grade
+        {
+            get => _grade;
+            set => Set(ref _grade, value);
+        }
+
+        private string _memo;
+        public string memo
+        {
+            get => _memo;
+            set => Set(ref _memo, value);
+        }
+    }
+}
