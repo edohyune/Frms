039d733a479be47233aaac844fa4fda3971ca8e1
nophominkham <165004618+nophominkham@users.noreply.github.com>
2024-09-16 16:16:30 +0700

    N Update Model F30 "AppRegister"

---
+++ AppRegister/F30.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicV004.Frms
+{
+    public class AppRegister_F30 : EpicV004.Libs.MdlBase
+    {
+        private string _applicant_id;
+        public string applicant_id
+        {
+            get => _applicant_id;
+            set => Set(ref _applicant_id, value);
+        }
+
+        private byte[] _photo;
+        public byte[] photo
+        {
+            get => _photo;
+            set => Set(ref _photo, value);
+        }
+
+        private byte[] _photo2;
+        public byte[] photo2
+        {
+            get => _photo2;
+            set => Set(ref _photo2, value);
+        }
+    }
+}
